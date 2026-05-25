18c9c711b5827921d05e876fe3221ae347618ffb
Miguel Ku <miguelkumartinez@gmail.com>
2026-05-25 11:53:48 -0600

    Agregando los ultimos metodos del controlador de artista

--- backend/backend/spotify_music/src/main/java/Model/controller/artist_new/ArtistController.java
+++ backend/backend/spotify_music/src/main/java/Model/controller/artist_new/ArtistController.java
@@ -1,11 +1,13 @@
 package Model.controller.artist_new;
 import Model.controller.artist_new.dto.request.RegisterArtistRequestDto;
+import Model.controller.artist_new.dto.response.ArtistResponseDto;
 import Model.controller.artist_new.service.ArtistService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -26,5 +28,21 @@ public class ArtistController {
         response.put("role", "Artista");
         
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/searchAll")
+    public ResponseEntity<List<ArtistResponseDto>> findAllArtists() {
+        List<ArtistResponseDto> artists = service.findAllArtists();
+
+        return ResponseEntity.ok(artists);
+    }
+
+    @GetMapping("/searchByName")
+    public ResponseEntity<List<ArtistResponseDto>> findAllArtistsByName(
+            @RequestParam String keyword
+    ) {
+        List<ArtistResponseDto> artists = service.findAllArtistsByName(keyword);
+
+        return ResponseEntity.ok(artists);
     }
 }
