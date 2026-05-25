a16a1cf6268aea0e901d8a03ce0f8b5914c39fb9
Miguel Ku <miguelkumartinez@gmail.com>
2026-05-25 11:43:58 -0600

    Agregando los metodos del controlador de generos musicales

--- backend/backend/spotify_music/src/main/java/Model/controller/music_genre_new/MusicGenreController.java
+++ backend/backend/spotify_music/src/main/java/Model/controller/music_genre_new/MusicGenreController.java
@@ -4,21 +4,41 @@ package Model.controller.music_genre_new;
 import Model.controller.music_genre_new.dto.request.CreateGenreRequestDto;
 import Model.controller.music_genre_new.dto.response.GenreResponseDto;
 import Model.controller.music_genre_new.service.MusicGenreService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/genres")
 public class MusicGenreController {
-    public final MusicGenreService musicGenreService;
+
+    private final MusicGenreService musicGenreService;
+
     public MusicGenreController(MusicGenreService musicGenreService) {
         this.musicGenreService = musicGenreService;
     }
-    public void createGenre(CreateGenreRequestDto dto){
-        musicGenreService.createGenre(dto);
-    }
-    public void createGenres(List<CreateGenreRequestDto> dtos){
-        musicGenreService.createGenres(dtos);
-    }
-    public List<GenreResponseDto> getAllGenresByName(String keyword){return musicGenreService.getAllGenresByName(keyword);}
-    public List<GenreResponseDto> getAllGenres(){return musicGenreService.getAllGenres();}
 
+    @PostMapping("/create")
+    public ResponseEntity<Void> createGenre(
+            @RequestBody CreateGenreRequestDto dto
+    ) {
+        musicGenreService.createGenre(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<GenreResponseDto>> getAllGenresByName(
+            @RequestParam String keyword
+    ) {
+        List<GenreResponseDto> genres = musicGenreService.getAllGenresByName(keyword);
+        return ResponseEntity.ok(genres);
+    }
+
+    @GetMapping("/searchAll")
+    public ResponseEntity<List<GenreResponseDto>> getAllGenres() {
+        List<GenreResponseDto> genres = musicGenreService.getAllGenres();
+        return ResponseEntity.ok(genres);
+    }
 }
