c0373b1b59efb08b8c666cd554a4027693cd1c1a
Miguel Ku <miguelkumartinez@gmail.com>
2026-05-28 19:39:03 -0600

    agregando el controlador del buscador

--- backend/backend/spotify_music/src/main/java/Model/controller/search/SearchController.java
+++ backend/backend/spotify_music/src/main/java/Model/controller/search/SearchController.java
@@ -1,4 +1,29 @@
 package Model.controller.search;
 
+import Model.controller.search.dto.response.SearchResponseDto;
+import Model.controller.search.service.SearchService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/search")
 public class SearchController {
+
+    private final SearchService service;
+
+    public SearchController(SearchService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<SearchResponseDto> generalSearch(@RequestParam String keyword) {
+
+        if (keyword == null || keyword.trim().isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        SearchResponseDto response = service.generalSearch(keyword.trim());
+
+        return ResponseEntity.ok(response);
+    }
 }
